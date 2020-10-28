1230b45fe36647f13fe7236092bdbbd8c2cc9a84
Cosmin <cdinro73@gmail.com>
2020-10-28 20:47:29 +0000

    test

--- src/IPv4.php
+++ src/IPv4.php
@@ -22,7 +22,7 @@ Class IPv4Addr {
 	}
 
 	function FormatRemoteAddr() {
-	        return "Your Remote IPV4 address is:" . $this->GetRemoteAddr();
+	        return "This is your Remote IPV4 address is:" . $this->GetRemoteAddr();
 	}	
 
 }
